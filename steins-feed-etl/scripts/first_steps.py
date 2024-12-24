81905d158430bfa60f9c6008fef3d162b418854d
Hans Yu <hansyu@tesla.com>
2024-12-25 00:18:17 +0100

    Configure logging.

--- steins-feed-etl/scripts/first_steps.py
+++ steins-feed-etl/scripts/first_steps.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 
 import asyncio
+import logging
 import os
 
 import aiohttp
@@ -8,9 +9,14 @@ import dotenv
 import sqlalchemy.orm as sqla_orm
 
 import steins_feed_etl.items
+import steins_feed_logging
 import steins_feed_model
 
 dotenv.load_dotenv()
+
+logger = steins_feed_logging.LoggerFactory.get_logger(steins_feed_etl.items.__name__)
+steins_feed_logging.LoggerFactory.add_stream_handler(logger)
+steins_feed_logging.LoggerFactory.set_level(logger, level=logging.DEBUG)
 
 async def main():
     engine = steins_feed_model.EngineFactory.get_or_create_engine(database=os.environ["DB_NAME"])
