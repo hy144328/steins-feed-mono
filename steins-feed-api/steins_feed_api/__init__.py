0a8590b5f1a89939077295dca0f7b3cd02a5deea
Hans Yu <hansyu@tesla.com>
2024-12-25 00:30:41 +0100

    Initialize engine.

--- steins-feed-api/steins_feed_api/__init__.py
+++ steins-feed-api/steins_feed_api/__init__.py
@@ -1,6 +1,22 @@
+#!/usr/bin/env python3
+
+import os
+
+import dotenv
 import fastapi
 
+import steins_feed_model
+
+dotenv.load_dotenv()
+
 app = fastapi.FastAPI()
+engine = steins_feed_model.EngineFactory.get_or_create_engine(
+    username = os.getenv("DB_USER"),
+    password = os.getenv("DB_PASS"),
+    host = os.getenv("DB_HOST"),
+    port = os.getenv("DB_PORT"),
+    database = os.getenv("DB_NAME"),
+)
 
 @app.get("/")
 async def root():
