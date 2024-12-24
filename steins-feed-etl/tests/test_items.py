d0a808c34f53867bc3c0d35b72c7ff8800233206
Hans Yu <hansyu@tesla.com>
2024-12-24 01:21:21 +0100

    First draft of unit test.

---
+++ steins-feed-etl/tests/test_items.py
@@ -0,0 +1,44 @@
+import asyncio
+import tempfile
+
+import pytest
+import sqlalchemy as sqla
+import sqlalchemy.orm as sqla_orm
+
+import steins_feed_config.feeds
+import steins_feed_etl.items
+import steins_feed_model
+import steins_feed_model.base
+
+@pytest.fixture
+def engine() -> sqla.engine.Engine:
+    return steins_feed_model.EngineFactory.get_or_create_engine()
+
+def test_parser_feeds(
+    engine: sqla.engine.Engine,
+):
+    steins_feed_model.base.Base.metadata.create_all(engine)
+
+    with tempfile.TemporaryDirectory() as temp_dir:
+        with tempfile.NamedTemporaryFile("w", dir=temp_dir, delete=False) as f:
+            f.write("""
+<root>
+  <feed>
+    <title>The Guardian</title>
+    <link>https://www.theguardian.com/uk/rss</link>
+    <lang>English</lang>
+  </feed>
+</root>
+            """)
+
+        with sqla_orm.Session(engine) as session:
+            with open(f.name, "r") as f:
+                steins_feed_config.feeds.read_xml(session, f)
+
+    with sqla_orm.Session(engine) as session:
+        asyncio.run(steins_feed_etl.items.parse_feeds(session))
+
+    q = sqla.select(steins_feed_model.items.Item)
+    with sqla_orm.Session(engine) as session:
+        res = session.execute(q).scalars().all()
+        assert len(res) > 0
