c015a08649f1ffe870dd19dacc0e8213db1770b0
Hans Yu <hansyu@tesla.com>
2024-12-25 10:16:57 +0100

    Pydantic model.

--- steins-feed-api/steins_feed_api/routers/items.py
+++ steins-feed-api/steins_feed_api/routers/items.py
@@ -1,6 +1,8 @@
 import datetime
+import typing
 
 import fastapi
+import pydantic
 import sqlalchemy as sqla
 import sqlalchemy.orm as sqla_orm
 
@@ -14,11 +16,62 @@ router = fastapi.APIRouter(
     tags = ["items"],
 )
 
+class Item(pydantic.BaseModel):
+    id: int
+    title: str
+    link: str
+    published: datetime.datetime
+    summary: typing.Optional[str]
+    feed: "Feed"
+    like: typing.Optional[steins_feed_model.items.LikeStatus]
+    magic: typing.Optional[float]
+
+    @classmethod
+    def from_model(cls, item: steins_feed_model.items.Item) -> "Item":
+        return Item(
+            id = item.id,
+            title = item.title,
+            link = item.link,
+            summary = item.summary,
+            published = item.published,
+            feed = Feed.from_model(item.feed),
+            like = item.likes[0].score if len(item.likes) > 0 else None,
+            magic = item.magic[0].score if len(item.magic) > 0 else None,
+        )
+
+class Feed(pydantic.BaseModel):
+    id: int
+    title: str
+    link: str
+    language: typing.Optional[steins_feed_model.feeds.Language]
+    tags: list["Tag"]
+
+    @classmethod
+    def from_model(cls, feed: steins_feed_model.feeds.Feed) -> "Feed":
+        return Feed(
+            id = feed.id,
+            title = feed.title,
+            link = feed.link,
+            language = feed.language,
+            tags =[Tag.from_model(tag_it) for tag_it in feed.tags],
+        )
+
+class Tag(pydantic.BaseModel):
+    id: int
+    name: str
+
+    @classmethod
+    def from_model(cls, tag: steins_feed_model.feeds.Tag) -> "Tag":
+        return Tag(
+            id = tag.id,
+            name = tag.name,
+        )
+
 @router.get("/")
 async def root(
     dt_from: datetime.datetime,
     dt_to: datetime.datetime,
-) -> list[steins_feed_model.items.Item]:
+) -> list[Item]:
     engine = steins_feed_model.EngineFactory.get_or_create_engine()
 
     q = sqla.select(
@@ -34,5 +87,7 @@ async def root(
     )
 
     with sqla_orm.Session(engine) as session:
-        res = session.execute(q).scalars().all()
-        return list(res)
+        return [
+            Item.from_model(item_it)
+            for item_it in session.execute(q).scalars()
+        ]
