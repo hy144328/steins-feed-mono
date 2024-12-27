fc69222b9fdb35c3d54862df0e1bb7e9694826ee
Hans Yu <hansyu@tesla.com>
2024-12-27 12:54:50 +0100

    Global CSS.

--- steins-feed-ui/app/layout.tsx
+++ steins-feed-ui/app/layout.tsx
@@ -1,3 +1,5 @@
+import "./global.css"
+
 export default function RootLayout({
   children,
 }: {
@@ -6,13 +8,6 @@ export default function RootLayout({
   return (
 <html lang="en">
   <head>
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css"/>
-    <link
-      rel="stylesheet"
-      href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css"
-      integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN"
-      crossOrigin="anonymous"
-    />
     <title>Stein&apos;s Feed</title>
   </head>
   <body>{children}</body>
