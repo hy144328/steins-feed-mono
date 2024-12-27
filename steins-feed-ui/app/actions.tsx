841066f4a2d47c218ef811bc54dc7d9385ffdaa3
Hans Yu <hansyu@tesla.com>
2024-12-27 21:10:06 +0100

    Introduce authenticate().

--- steins-feed-ui/app/actions.tsx
+++ steins-feed-ui/app/actions.tsx
@@ -1,12 +1,12 @@
 "use server"
 
-import { cookies } from "next/headers"
+import { Item, likeItemsLikePut, LikeStatus, rootItemsGet } from "@client"
 
-import { client, Item, likeItemsLikePut, LikeStatus, rootItemsGet } from "@client"
-
-client.setConfig({"baseUrl": process.env.API_BASE_URL});
+import { authenticate } from "./auth"
 
 export async function doLikeItemsLikePut(item: Item, score: LikeStatus) {
+  await authenticate();
+
   const resp = await likeItemsLikePut({
     "query": {
       "item_id": item.id,
@@ -23,28 +23,18 @@ export async function doRootItemsGet(
   dt_from: Date,
   dt_to: Date,
 ): Promise<Item[]> {
-  const cookie_store = await cookies();
-  const cookie = cookie_store.get("api_token");
+  await authenticate();
 
-  if (!cookie) {
-    throw {"detail": "Not authenticated"};
-  }
-
-  client.interceptors.request.use(request => {
-    request.headers.set("Authorization", `Bearer ${cookie.value}`);
-    return request;
-  });
-
-  const items_response = await rootItemsGet({
+  const resp = await rootItemsGet({
     "query": {
       "dt_from": dt_from.toISOString(),
       "dt_to": dt_to.toISOString(),
     },
   });
 
-  if (items_response.error) {
-    throw items_response.error;
+  if (resp.error) {
+    throw resp.error;
   }
 
-  return items_response.data;
+  return resp.data;
 }
--- steins-feed-ui/app/auth.tsx
+++ steins-feed-ui/app/auth.tsx
@@ -2,7 +2,25 @@
 
 import { cookies } from "next/headers"
 
+import { client } from "@client"
+
+client.setConfig({"baseUrl": process.env.API_BASE_URL});
+
 export async function logout() {
   const cookie_store = await cookies();
   cookie_store.delete("api_token");
+}
+
+export async function authenticate() {
+  const cookie_store = await cookies();
+  const cookie = cookie_store.get("api_token");
+
+  if (!cookie) {
+    throw {"detail": "Not authenticated"};
+  }
+
+  client.interceptors.request.use(request => {
+    request.headers.set("Authorization", `Bearer ${cookie.value}`);
+    return request;
+  });
 }
