4296699014ecb95182440f49da1f0ec7fd170ae0
Hans Yu <hansyu@tesla.com>
2024-12-28 23:33:59 +0100

    Streamline TopNav.

--- steins-feed-ui/app/navigation.tsx
+++ steins-feed-ui/app/navigation.tsx
@@ -2,17 +2,7 @@
 
 import { useRouter } from "next/navigation"
 import { useState } from "react"
-import Button from "react-bootstrap/Button"
-import ButtonGroup from "react-bootstrap/ButtonGroup"
-import Col from "react-bootstrap/Col"
-import Container from "react-bootstrap/Container"
-import Nav from "react-bootstrap/Nav"
-import Navbar from "react-bootstrap/Navbar"
-import NavbarBrand from "react-bootstrap/NavbarBrand"
-import NavItem from "react-bootstrap/NavItem"
-import NavLink from "react-bootstrap/NavLink"
 import Offcanvas from "react-bootstrap/Offcanvas"
-import Row from "react-bootstrap/Row"
 
 import { logout } from "./auth"
 
@@ -52,42 +42,43 @@ function TopNav({
   }
 
   return (
-<Navbar data-bs-theme="dark" bg="dark" sticky="top">
-<Container>
-<NavbarBrand>
+<nav className="navbar bg-dark sticky-top" data-bs-theme="dark">
+<div className="container">
+<a className="navbar-brand">
 <img
   src="/apple-touch-icon.png"
-  style={ {display: "inline-block", height: "1.5em", verticalAlign: "top", width: "1.5em"} }
+  className="d-inline-block align-text-top"
+  style={ {height: "1.5em", width: "1.5em"} }
   alt="Stein's Feed logo"
 />
 &nbsp;
 Stein&apos;s Feed
-</NavbarBrand>
-<Nav variant="underline">
-<NavItem><NavLink href="/" active>Home</NavLink></NavItem>
-<NavItem><NavLink disabled>Settings</NavLink></NavItem>
-<NavItem><NavLink disabled>Statistics</NavLink></NavItem>
-</Nav>
-<Nav>
-<NavItem>
-<ButtonGroup>
-<Button href={ `/?now=${encodeURIComponent(tomorrow.toISOString())}` }><i className="bi-rewind-fill"/></Button>
-<Button disabled><i className="bi-caret-up-fill"/></Button>
-<Button disabled><i className="bi-caret-down-fill"/></Button>
-<Button href={ `/?now=${encodeURIComponent(yesterday.toISOString())}` }><i className="bi-fast-forward-fill"/></Button>
-</ButtonGroup>
-</NavItem>
-</Nav>
-<Nav>
-<NavItem>
-<ButtonGroup>
-<Button variant="danger"><i className="bi-power" onClick={ handleLogout }/></Button>
-<Button><i className="bi-list" onClick={ () => setShow(true) }/></Button>
-</ButtonGroup>
-</NavItem>
-</Nav>
-</Container>
-</Navbar>
+</a>
+<ul className="nav nav-underline">
+<li className="nav-item"><a className="nav-link active" href="/">Home</a></li>
+<li className="nav-item"><a className="nav-link disabled">Settings</a></li>
+<li className="nav-item"><a className="nav-link disabled">Statistics</a></li>
+</ul>
+<ul className="nav">
+<li className="nav-item">
+<div className="btn-group">
+<a className="btn btn-primary" href={ `/?now=${encodeURIComponent(tomorrow.toISOString())}` }><i className="bi-rewind-fill"/></a>
+<button className="btn btn-primary" disabled><i className="bi-caret-up-fill"/></button>
+<button className="btn btn-primary" disabled><i className="bi-caret-down-fill"/></button>
+<a className="btn btn-primary" href={ `/?now=${encodeURIComponent(yesterday.toISOString())}` }><i className="bi-fast-forward-fill"/></a>
+</div>
+</li>
+</ul>
+<ul className="nav">
+<li className="nav-item">
+<button className="btn btn-danger"><i className="bi-power" onClick={ handleLogout }/></button>
+</li>
+<li className="nav-item">
+<button className="btn btn-primary"><i className="bi-list" onClick={ () => setShow(true) }/></button>
+</li>
+</ul>
+</div>
+</nav>
   );
 }
 
@@ -107,49 +98,49 @@ function SideNav({
     <form>
     <fieldset style={ {all: "revert"} }>
     <legend style={ {all: "revert"} }>Languages</legend>
-    <Row>
-    <Col xs={ 1 }>
+    <div className="row">
+    <div className="col-1">
     <input type="checkbox"/>
-    </Col>
-    <Col>
+    </div>
+    <div className="col">
     <label htmlFor="foo">English</label>
-    </Col>
-    </Row>
-    <Row>
-    <Col xs={ 1 }>
+    </div>
+    </div>
+    <div className="row">
+    <div className="col-1">
     <input type="checkbox"/>
-    </Col>
-    <Col>
+    </div>
+    <div className="col">
     <label>German</label>
-    </Col>
-    </Row>
-    <Row>
-    <Col xs={ 1 }>
+    </div>
+    </div>
+    <div className="row">
+    <div className="col-1">
     <input type="checkbox"/>
-    </Col>
-    <Col>
+    </div>
+    <div className="col">
     <label>Swedish</label>
-    </Col>
-    </Row>
+    </div>
+    </div>
     </fieldset>
     <fieldset style={ {all: "revert"} }>
     <legend style={ {all: "revert"} }>Tags</legend>
-    <Row>
-    <Col xs={ 1 }>
+    <div className="row">
+    <div className="col-1">
     <input type="checkbox"/>
-    </Col>
-    <Col>
+    </div>
+    <div className="col">
     <label>news</label>
-    </Col>
-    </Row>
-    <Row>
-    <Col xs={ 1 }>
+    </div>
+    </div>
+    <div className="row">
+    <div className="col-1">
     <input type="checkbox"/>
-    </Col>
-    <Col>
+    </div>
+    <div className="col">
     <label>magazines</label>
-    </Col>
-    </Row>
+    </div>
+    </div>
     </fieldset>
     </form>
   </Offcanvas.Body>
