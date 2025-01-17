8a9cb11b0ef6a74fe4f7194a3305dea64fa75285
Hans Yu <hans.yu@outlook.de>
2025-01-17 02:25:43 +0100

    Tag actions.

--- steins-feed-ui/app/feed/[feed_id]/actions.ts
+++ steins-feed-ui/app/feed/[feed_id]/actions.ts
@@ -1,10 +1,48 @@
 "use server"
 
 import { Tag } from "@client"
+import { attachTagFeedsFeedFeedIdAttachTagPut, createAndAttachTagFeedsFeedFeedIdCreateAndAttachTagPut, detachTagFeedsFeedFeedIdDetachTagDelete } from "@client"
 
-export async function submit_tag(tag_name: string): Promise<Tag> {
-  return {
-    id: -1,
-    name: tag_name,
+export async function doCreateAndAttachTag(
+  feed_id: number,
+  tag_name: string,
+): Promise<Tag> {
+  const resp = await createAndAttachTagFeedsFeedFeedIdCreateAndAttachTagPut({
+    path: {feed_id: feed_id},
+    query: {tag_name: tag_name},
+  });
+
+  if (resp.error) {
+    throw resp.error;
+  }
+
+  return resp.data;
+}
+
+export async function doAttachTag(
+  feed_id: number,
+  tag_id: number,
+) {
+  const resp = await attachTagFeedsFeedFeedIdAttachTagPut({
+    path: {feed_id: feed_id},
+    query: {tag_id: tag_id},
+  });
+
+  if (resp.error) {
+    throw resp.error;
+  }
+}
+
+export async function doDetachTag(
+  feed_id: number,
+  tag_id: number,
+) {
+  const resp = await detachTagFeedsFeedFeedIdDetachTagDelete({
+    path: {feed_id: feed_id},
+    query: {tag_id: tag_id},
+  });
+
+  if (resp.error) {
+    throw resp.error;
   }
 }
