720815db555f83f1aafc7f6b15e1e79ed9f7ca1a
Hans Yu <hans.yu@outlook.de>
2025-01-17 19:30:15 +0100

    Require authentication.

--- steins-feed-ui/app/feed/[feed_id]/actions.ts
+++ steins-feed-ui/app/feed/[feed_id]/actions.ts
@@ -3,10 +3,14 @@
 import { Tag } from "@client"
 import { attachTagFeedsFeedFeedIdAttachTagPut, createAndAttachTagFeedsFeedFeedIdCreateAndAttachTagPut, detachTagFeedsFeedFeedIdDetachTagDelete } from "@client"
 
+import { authenticate } from "../../auth"
+
 export async function doCreateAndAttachTag(
   feed_id: number,
   tag_name: string,
 ): Promise<Tag> {
+  await authenticate();
+
   const resp = await createAndAttachTagFeedsFeedFeedIdCreateAndAttachTagPut({
     path: {feed_id: feed_id},
     query: {tag_name: tag_name},
@@ -23,6 +27,8 @@ export async function doAttachTag(
   feed_id: number,
   tag_id: number,
 ) {
+  await authenticate();
+
   const resp = await attachTagFeedsFeedFeedIdAttachTagPut({
     path: {feed_id: feed_id},
     query: {tag_id: tag_id},
@@ -37,6 +43,8 @@ export async function doDetachTag(
   feed_id: number,
   tag_id: number,
 ) {
+  await authenticate();
+
   const resp = await detachTagFeedsFeedFeedIdDetachTagDelete({
     path: {feed_id: feed_id},
     query: {tag_id: tag_id},
