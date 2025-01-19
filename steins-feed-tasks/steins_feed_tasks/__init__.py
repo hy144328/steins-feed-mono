e1d4d80dffdb5bcd721bbff4d21a3189305e583a
Hans Yu <hans.yu@outlook.de>
2025-01-19 12:53:20 +0100

    Move logic and imports inside tasks.

--- steins-feed-tasks/steins_feed_tasks/__init__.py
+++ steins-feed-tasks/steins_feed_tasks/__init__.py
@@ -1,17 +1,9 @@
 #!/usr/bin/env python3
 
-import asyncio
-import logging
 import os
 
-import aiohttp
 import celery
 import dotenv
-import sqlalchemy.orm as sqla_orm
-
-import steins_feed_etl.items
-import steins_feed_logging
-import steins_feed_model
 
 dotenv.load_dotenv()
 
@@ -21,25 +13,36 @@ app = celery.Celery(
     backend = os.getenv("RESULT_BACKEND"),
 )
 
-try:
-    os.mkdir("logs.d")
-except FileExistsError:
-    pass
-
-with open("logs.d/steins_feed_etl.log", "a") as f:
-    etl_logger = steins_feed_logging.LoggerFactory.get_logger(steins_feed_etl.items.__name__)
-    steins_feed_logging.LoggerFactory.add_file_handler(etl_logger, f)
-    steins_feed_logging.LoggerFactory.set_level(etl_logger, level=logging.INFO)
-
 @app.task
 def add(x, y):
     return x + y
 
 @app.task
 def parse_feeds():
+    import asyncio
+
     asyncio.run(parse_feeds_async())
 
 async def parse_feeds_async():
+    import logging
+
+    import aiohttp
+    import sqlalchemy.orm as sqla_orm
+
+    import steins_feed_etl.items
+    import steins_feed_logging
+    import steins_feed_model
+
+    try:
+        os.mkdir("logs.d")
+    except FileExistsError:
+        pass
+
+    with open("logs.d/steins_feed_etl.log", "a") as f:
+        etl_logger = steins_feed_logging.LoggerFactory.get_logger(steins_feed_etl.items.__name__)
+        steins_feed_logging.LoggerFactory.add_file_handler(etl_logger, f)
+        steins_feed_logging.LoggerFactory.set_level(etl_logger, level=logging.INFO)
+
     engine = steins_feed_model.EngineFactory.get_or_create_engine(database=os.environ["DB_NAME"])
     connector = aiohttp.TCPConnector(limit=5, limit_per_host=1)
 
