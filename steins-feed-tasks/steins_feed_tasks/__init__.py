c5c251eca6564c004ac3f5be1c719f8a12e51270
Hans Yu <hans.yu@outlook.de>
2025-01-19 13:08:19 +0100

    Introduce modules.

--- steins-feed-tasks/steins_feed_tasks/__init__.py
+++ steins-feed-tasks/steins_feed_tasks/__init__.py
@@ -1,51 +1,10 @@
 #!/usr/bin/env python3
 
-import os
-
-import celery
 import dotenv
 
 dotenv.load_dotenv()
 
-app = celery.Celery(
-    __name__,
-    broker = os.environ["BROKER_URL"],
-    backend = os.getenv("RESULT_BACKEND"),
-)
+from .app import app
 
-@app.task
-def add(x, y):
-    return x + y
-
-@app.task
-def parse_feeds():
-    import asyncio
-
-    asyncio.run(parse_feeds_async())
-
-async def parse_feeds_async():
-    import logging
-
-    import aiohttp
-    import sqlalchemy.orm as sqla_orm
-
-    import steins_feed_etl.items
-    import steins_feed_logging
-    import steins_feed_model
-
-    try:
-        os.mkdir("logs.d")
-    except FileExistsError:
-        pass
-
-    with open("logs.d/steins_feed_etl.log", "a") as f:
-        etl_logger = steins_feed_logging.LoggerFactory.get_logger(steins_feed_etl.items.__name__)
-        steins_feed_logging.LoggerFactory.add_file_handler(etl_logger, f)
-        steins_feed_logging.LoggerFactory.set_level(etl_logger, level=logging.INFO)
-
-    engine = steins_feed_model.EngineFactory.get_or_create_engine(database=os.environ["DB_NAME"])
-    connector = aiohttp.TCPConnector(limit=5, limit_per_host=1)
-
-    with sqla_orm.Session(engine) as session:
-        async with aiohttp.ClientSession(connector=connector) as client:
-            await steins_feed_etl.items.parse_feeds(session, client, skip_recent=True)
+if __name__ == "__main__":
+    app.start()
