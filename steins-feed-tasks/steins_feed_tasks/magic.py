c30a250070fc449e1b8032d443fc97a3b8bd940d
Hans Yu <hans.yu@outlook.de>
2025-01-21 00:18:40 +0100

    First draft of score update.

--- steins-feed-tasks/steins_feed_tasks/magic.py
+++ steins-feed-tasks/steins_feed_tasks/magic.py
@@ -1,5 +1,8 @@
 import typing
 
+import celery
+import celery.result
+
 from .app import app
 
 if typing.TYPE_CHECKING:
@@ -44,9 +47,7 @@ def train_classifier(
     log.magic_logger.info(f"Finish train_classifier: {user_id}, {lang}.")
 
 @app.task
-def train_classifiers_all():
-    import celery
-    import celery.result
+def train_classifiers_all() -> celery.result.GroupResult:
     import sqlalchemy as sqla
     import sqlalchemy.orm as sqla_orm
 
@@ -71,3 +72,77 @@ def train_classifiers_all():
     log.magic_logger.info("Finish train_classifiers_all.")
     assert isinstance(res, celery.result.GroupResult)
     return res
+
+@app.task
+def update_scores(
+    user_id: int,
+    item_ids: typing.Sequence[int],
+):
+    import itertools
+
+    import sqlalchemy as sqla
+    import sqlalchemy.orm as sqla_orm
+
+    import steins_feed_magic
+    import steins_feed_magic.io
+
+    from . import db
+    from . import log
+
+    log.magic_logger.info(f"Start update_scores for {user_id}.")
+
+    with sqla_orm.Session(db.engine) as session:
+        all_items = (
+            session.get_one(
+                steins_feed_model.items.Item,
+                item_id,
+                options = [
+                    sqla_orm.joinedload(steins_feed_model.items.Item.feed),
+                    sqla_orm.joinedload(
+                        steins_feed_model.items.Item.magic.and_(
+                            steins_feed_model.items.Magic.user_id == user_id,
+                        ),
+                    ),
+                ],
+            )
+            for item_id in item_ids
+        )
+        unscored_items = (
+            item_it
+            for item_it in all_items
+            if item_it.feed.language is not None and len(item_it.magic) > 0
+        )
+        grouped_items = itertools.groupby(unscored_items, key=lambda x: x.feed.language)
+
+        q = sqla.insert(steins_feed_model.items.Magic)
+        q = q.prefix_with("OR IGNORE", dialect="sqlite")
+
+        for k, vs in grouped_items:
+            vs = list(vs)
+
+            if k is None:
+                log.magic_logger.warning(f"Skip {len(vs)} items without language.")
+                continue
+
+            try:
+                clf = steins_feed_magic.io.read_classifier(user_id, k)
+            except FileNotFoundError:
+                log.magic_logger.warning(f"Skip {len(vs)} {k} items without classifier.")
+                continue
+
+            scores = steins_feed_magic.predict_scores(clf, vs)
+            res = [
+                {
+                    "user_id": user_id,
+                    "item_id": item_it.id,
+                    "score": score_it,
+                }
+                for item_it, score_it in zip(vs, scores)
+            ]
+
+            log.magic_logger.info(f"Update scores of {len(vs)} {k} items.")
+            session.execute(q, res)
+
+        session.commit()
+
+    log.magic_logger.info(f"Finish update_scores for {user_id}.")
