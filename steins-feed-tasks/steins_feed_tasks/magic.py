3d26cfb0f86f0df0078a3da6b2fa77277614006d
Hans Yu <hans.yu@outlook.de>
2025-01-22 22:41:32 +0100

    Major refactor.

--- steins-feed-tasks/steins_feed_tasks/magic.py
+++ steins-feed-tasks/steins_feed_tasks/magic.py
@@ -72,74 +72,11 @@ def train_classifiers_all():
     log.magic_logger.info("Finish train_classifiers_all.")
 
 @app.task
-def update_scores(
-    user_id: int,
+def calculate_scores(
     item_ids: typing.Sequence[int],
-) -> "celery.result.GroupResult":
-    import itertools
-
-    import celery
-
-    import sqlalchemy as sqla
-    import sqlalchemy.orm as sqla_orm
-
-    import steins_feed_model.items
-
-    from . import db
-    from . import log
-
-    log.magic_logger.info(f"Start to update scores for {user_id}.")
-
-    q = sqla.select(
-        steins_feed_model.items.Item.id,
-        steins_feed_model.feeds.Feed.language,
-    ).join(
-        steins_feed_model.items.Item.feed,
-    ).join(
-        steins_feed_model.items.Item.magic.and_(
-            steins_feed_model.items.Magic.user_id == user_id,
-        ),
-        isouter = True,
-    ).where(
-        steins_feed_model.items.Item.id.in_(item_ids),
-        steins_feed_model.items.Magic.item_id == None,
-    ).order_by(
-        steins_feed_model.feeds.Feed.language,
-    )
-
-    with sqla_orm.Session(db.engine) as session:
-        items = itertools.groupby(
-            session.execute(q).tuples(),
-            key = lambda x: x[1],
-        )
-
-        assert isinstance(update_scores_by_language, celery.Task)
-        signatures = []
-
-        for lang_it, items_it in items:
-            if lang_it is None:
-                log.magic_logger.warning(f"Skip {len(list(items_it))} items without language.")
-                continue
-
-            sign_it = update_scores_by_language.s(
-                user_id = user_id,
-                item_ids = [item_it[0] for item_it in items_it],
-                lang = lang_it,
-            )
-            signatures.append(sign_it)
-
-    job = celery.group(*signatures)
-    res = job()
-
-    log.magic_logger.info(f"Finish to update scores for {user_id}.")
-    return res
-
-@app.task
-def update_scores_by_language(
     user_id: int,
-    item_ids: typing.Sequence[int],
     lang: "steins_feed_model.feeds.Language | str",
-):
+) -> list[typing.Tuple[int, float]] | list[typing.Tuple[int, None]]:
     import sqlalchemy as sqla
     import sqlalchemy.orm as sqla_orm
 
@@ -150,7 +87,7 @@ def update_scores_by_language(
     from . import db
     from . import log
 
-    log.magic_logger.info(f"Start to update scores for {user_id} and {lang}.")
+    log.magic_logger.info(f"Start to calculate scores for {user_id} and {lang}.")
 
     if not isinstance(lang, steins_feed_model.feeds.Language):
         lang = steins_feed_model.feeds.Language(lang)
@@ -159,32 +96,57 @@ def update_scores_by_language(
         clf = steins_feed_magic.io.read_classifier(user_id, lang)
     except FileNotFoundError:
         log.magic_logger.warning(f"Skip {len(item_ids)} {lang} items without classifier.")
-        return
+        return [(item_id, None) for item_id in item_ids]
 
     q = sqla.select(
         steins_feed_model.items.Item,
     ).where(
         steins_feed_model.items.Item.id.in_(item_ids),
     )
+    log.magic_logger.info(f"Calculate scores of {len(item_ids)} {lang} items.")
 
     with sqla_orm.Session(db.engine) as session:
         items = session.execute(q).scalars().all()
         scores = steins_feed_magic.predict_scores(clf, items)
         res = [
-            {
-                "user_id": user_id,
-                "item_id": item_it.id,
-                "score": score_it,
-            }
+            (item_it.id, score_it)
             for item_it, score_it in zip(items, scores)
         ]
 
-    stmt = sqla.insert(steins_feed_model.items.Magic)
-    stmt = stmt.prefix_with("OR IGNORE", dialect="sqlite")
+    log.magic_logger.info(f"Finish to calculate scores for {user_id} and {lang}.")
+    return res
+
+@app.task
+def update_scores(
+    item_scores: typing.Sequence[typing.Tuple[int, float]],
+    user_id: int,
+    lang: "steins_feed_model.feeds.Language | str",
+):
+    import sqlalchemy as sqla
+    import sqlalchemy.orm as sqla_orm
+
+    import steins_feed_model.items
+
+    from . import db
+    from . import log
+
+    log.magic_logger.info(f"Start to update scores for {user_id} and {lang}.")
+
+    q = sqla.insert(steins_feed_model.items.Magic)
+    q = q.prefix_with("OR IGNORE", dialect="sqlite")
+
+    res = [
+        {
+            "user_id": user_id,
+            "item_id": item_id,
+            "score": score_it,
+        }
+        for item_id, score_it in item_scores
+    ]
 
     with sqla_orm.Session(db.engine) as session:
-        log.magic_logger.info(f"Update scores of {len(items)} {lang} items.")
-        session.execute(stmt, res)
+        log.magic_logger.info(f"Update scores of {len(item_scores)} {lang} items.")
+        session.execute(q, res)
         session.commit()
 
     log.magic_logger.info(f"Finish to update scores for {user_id} and {lang}.")
