f063be33a98984949c489730c7f66c73e0543038
Hans Yu <hans.yu@outlook.de>
2025-01-28 10:28:39 +0100

    Reservoir returns value on success.

--- steins-feed-math/steins_feed_math/statistics/__init__.py
+++ steins-feed-math/steins_feed_math/statistics/__init__.py
@@ -1,4 +1,5 @@
 import random
+import typing
 
 class Reservoir[T]:
     def __init__(
@@ -13,12 +14,12 @@ class Reservoir[T]:
         self.sample: list[T] = []
         self.sample_weights: list[float] = []
 
-    def add(self, value: T, weight: float):
+    def add(self, value: T, weight: float=1) -> typing.Optional[T]:
         if weight < 0:
             raise ValueError("Weight has to be non-negative.")
 
         if weight == 0:
-            return
+            return None
 
         self.n += weight
 
@@ -29,6 +30,10 @@ class Reservoir[T]:
             i = self._choice()
             self.sample[i] = value
             self.sample_weights[i] = weight
+        else:
+            return None
+
+        return value
 
     def _choice(self) -> int:
         idxs = self.rng.choices(range(len(self.sample)), weights=self.sample_weights)
