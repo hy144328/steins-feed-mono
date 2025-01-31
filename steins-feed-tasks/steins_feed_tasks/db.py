5c232992f358b8f32efa2708923c36eaa01b1e0d
Hans Yu <hans.yu@outlook.de>
2025-01-31 12:23:05 +0100

    Update tasks.

--- steins-feed-tasks/steins_feed_tasks/db.py
+++ steins-feed-tasks/steins_feed_tasks/db.py
@@ -2,4 +2,10 @@ import os
 
 import steins_feed_model
 
-engine = steins_feed_model.EngineFactory.get_or_create_engine(database=os.environ["DB_NAME"])
+engine = steins_feed_model.EngineFactory.create_engine(
+    username = os.getenv("DB_USER"),
+    password = os.getenv("DB_PASS"),
+    host = os.getenv("DB_HOST"),
+    port = os.getenv("DB_PORT"),
+    database = os.environ["DB_NAME"],
+)
