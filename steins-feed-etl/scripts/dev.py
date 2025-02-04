aa60fae525557bee08e64387dc329b7891a151f7
Hans Yu <hans.yu@outlook.de>
2025-02-04 16:32:37 +0100

    Update scripts.

--- steins-feed-etl/scripts/dev.py
+++ steins-feed-etl/scripts/dev.py
@@ -18,11 +18,20 @@ with open(os.path.join(os.path.dirname(__file__), "dev_logging.toml"), "rb") as 
     logging.config.dictConfig(tomllib.load(f))
 
 async def main():
-    engine = steins_feed_model.EngineFactory.create_engine(database=os.environ["DB_NAME"])
+    engine = steins_feed_model.EngineFactory.create_engine(
+        username = os.getenv("DB_USER"),
+        password = os.getenv("DB_PASS"),
+        host = os.getenv("DB_HOST"),
+        port = os.getenv("DB_PORT"),
+        database = os.getenv("DB_NAME"),
+    )
 
     with sqla_orm.Session(engine) as session:
         async with aiohttp.ClientSession() as client:
-            await steins_feed_etl.items.parse_feeds(session, client)
+            await steins_feed_etl.items.parse_feeds(
+                session,
+                client,
+            )
 
 if __name__ == "__main__":
     asyncio.run(main())
--- steins-feed-etl/scripts/prod.py
+++ steins-feed-etl/scripts/prod.py
@@ -18,12 +18,22 @@ with open(os.path.join(os.path.dirname(__file__), "prod_logging.toml"), "rb") as
     logging.config.dictConfig(tomllib.load(f))
 
 async def main():
-    engine = steins_feed_model.EngineFactory.create_engine(database=os.environ["DB_NAME"])
+    engine = steins_feed_model.EngineFactory.create_engine(
+        username = os.getenv("DB_USER"),
+        password = os.getenv("DB_PASS"),
+        host = os.getenv("DB_HOST"),
+        port = os.getenv("DB_PORT"),
+        database = os.getenv("DB_NAME"),
+    )
     connector = aiohttp.TCPConnector(limit=5, limit_per_host=1)
 
     with sqla_orm.Session(engine) as session:
         async with aiohttp.ClientSession(connector=connector) as client:
-            await steins_feed_etl.items.parse_feeds(session, client, skip_recent=True)
+            await steins_feed_etl.items.parse_feeds(
+                session,
+                client,
+                skip_recent = True,
+            )
 
 if __name__ == "__main__":
     asyncio.run(main())
