e88d9098d510f1eeb87ec8172b5117260915d429
Hans Yu <hans.yu@outlook.de>
2025-02-04 02:23:07 +0100

    Update test.

--- steins-feed-config/tests/test_feeds.py
+++ steins-feed-config/tests/test_feeds.py
@@ -1,4 +1,5 @@
 import tempfile
+import typing
 
 import lxml.etree
 import pytest
@@ -12,29 +13,35 @@ import steins_feed_model.users
 
 @pytest.fixture
 def engine() -> sqla.engine.Engine:
-    return steins_feed_model.EngineFactory.create_engine()
-
-def test_read_and_write_xml(engine: sqla.engine.Engine):
+    engine = steins_feed_model.EngineFactory.create_engine()
     steins_feed_model.base.Base.metadata.create_all(engine)
+    return engine
 
+@pytest.fixture
+def session(
+    engine: sqla.engine.Engine,
+) -> typing.Generator[sqla_orm.Session]:
+    with sqla_orm.Session(engine) as session:
+        yield session
+
+@pytest.fixture
+def user(
+    session: sqla_orm.Session,
+) -> steins_feed_model.users.User:
     user = steins_feed_model.users.User(
         name = "hansolo",
         password = "",
         email = "hans.yu@outlook.de",
     )
+    session.add(user)
+    session.commit()
 
-    q = sqla.insert(
-        steins_feed_model.users.User,
-    ).values(
-        name = user.name,
-        password = user.password,
-        email = user.email,
-    )
+    return user
 
-    with sqla_orm.Session(engine) as session:
-        session.execute(q)
-        session.commit()
-
+def test_read_and_write_xml(
+    session: sqla_orm.Session,
+    user: steins_feed_model.users.User,
+):
     with tempfile.TemporaryDirectory() as temp_dir:
         with tempfile.NamedTemporaryFile("w", dir=temp_dir, delete=False) as f:
             f.write("""
@@ -43,33 +50,31 @@ def test_read_and_write_xml(engine: sqla.engine.Engine):
     <title>The Guardian</title>
     <link>https://www.theguardian.com/uk/rss</link>
     <lang>English</lang>
+    <tag>News</tag>
   </feed>
 </root>
             """)
 
-        with sqla_orm.Session(engine) as session:
-            with open(f.name, "r") as f:
-                steins_feed_config.read_xml(
-                    session,
-                    f,
-                    user_name = "hansolo",
-                )
+        with open(f.name, "r") as f:
+            steins_feed_config.read_xml(
+                session,
+                f,
+                user_name = "hansolo",
+            )
 
         q = sqla.select(steins_feed_model.feeds.Feed)
-        with sqla_orm.Session(engine) as session:
-            res = session.execute(q).scalars().all()
-            assert len(res) == 1
+        res = session.execute(q).scalars().all()
+        assert len(res) == 1
 
-        with sqla_orm.Session(engine) as session:
-            with tempfile.NamedTemporaryFile("w", dir=temp_dir, delete=False) as f:
-                pass
+        with tempfile.NamedTemporaryFile("w", dir=temp_dir, delete=False) as f:
+            pass
 
-            with open(f.name, "w") as f:
-                steins_feed_config.write_xml(
-                    session,
-                    f,
-                    user_name = "hansolo",
-                )
+        with open(f.name, "w") as f:
+            steins_feed_config.write_xml(
+                session,
+                f,
+                user_name = "hansolo",
+            )
 
         with open(f.name, "r") as f:
             tree = lxml.etree.parse(f)
