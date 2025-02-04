ac5ac53db05148e64480c670821703df93454862
Hans Yu <hans.yu@outlook.de>
2025-02-04 20:19:33 +0100

    First draft of custom implementation.

--- steins-feed-magic/steins_feed_magic/parse.py
+++ steins-feed-magic/steins_feed_magic/parse.py
@@ -1,11 +1,33 @@
+import collections
+
 import lxml.etree
 import lxml.html
 
+class HtmlElementTail(lxml.html.HtmlElement):
+    def __init__(self, text: str):
+        super().__init__()
+        self.text = text
+
 def text_content(s: str) -> str:
+    que = collections.deque[lxml.html.HtmlElement]()
+    res = []
+
     try:
         tree = lxml.html.fromstring(s)
-        res = tree.text_content()
+        que.append(tree)
     except lxml.etree.ParserError:  # pragma: no cover
-        res = ""
+        return ""
 
-    return res
+    while len(que) > 0:
+        node_it = que.pop()
+
+        if node_it.text is not None:
+            res.append(node_it.text)
+
+        for child_it in reversed(node_it):
+            if child_it.tail is not None:
+                que.append(HtmlElementTail(child_it.tail))
+
+            que.append(child_it)
+
+    return "".join(res)
