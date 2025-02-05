ad41280c4ef0015c9bdf6660d11df777d8a3b9fb
Hans Yu <hans.yu@outlook.de>
2025-02-05 15:48:07 +0100

    First draft of replace_word().

--- steins-feed-ui/__tests__/util.parse.test.ts
+++ steins-feed-ui/__tests__/util.parse.test.ts
@@ -1,7 +1,33 @@
-import {text_content} from "@parse"
+import {text_content, replace_word} from "@parse"
 
-test("text_content()", () => {
+test("text_content() works", () => {
   const s = "Hello <em>world</em>!";
   const t = "Hello world!";
   expect(text_content(s)).toBe(t);
+});
+
+test("replace_word() works with simple replacement", () => {
+  function emphasize(word: string): HTMLSpanElement {
+    const res = document.createElement("span");
+    res.textContent = word;
+    return res;
+  }
+
+  const s = "Hello <em>world</em>!";
+  const t = "H<span>ell</span>o <em>world</em>!";
+
+  expect(replace_word(s, "ell", emphasize)).toBe(t);
+});
+
+test("replace_word() works with complex replacement", () => {
+  function emphasize(word: string): HTMLSpanElement {
+    const res = document.createElement("span");
+    res.textContent = word;
+    return res;
+  }
+
+  const s = "Hello <em>world</em>!";
+  const t = "Hell<span>o </span><em><span>world</span></em><span>!</span>";
+
+  expect(replace_word(s, "o world!", emphasize)).toBe(t);
 });
--- steins-feed-ui/util/parse.ts
+++ steins-feed-ui/util/parse.ts
@@ -1,8 +1,73 @@
 export function text_content(s: string): string {
+  const res: Text[] = [];
+
+  for (const node_it of iterate_deep(parse_body(s))) {
+    if (node_it.nodeType === Node.TEXT_NODE) {
+      res.push(node_it as Text);
+    }
+  }
+
+  return res.map(x => x.nodeValue).join("");
+}
+
+export function replace_word(
+  s: string,
+  word: string,
+  wrap: (arg0: string) => Node,
+): string {
+  const body = parse_body(s);
+  const res: {node: Text, start: number, finish: number}[] = [];
+  let last_idx = 0;
+
+  for (const node_it of iterate_deep(body)) {
+    if (node_it.nodeType !== Node.TEXT_NODE) {
+      continue;
+    }
+
+    const res_it = {
+      node: node_it as Text,
+      start: last_idx,
+      finish: last_idx + node_it.nodeValue!.length,
+    };
+
+    res.push(res_it);
+    last_idx += node_it.nodeValue!.length;
+  }
+
+  const full_text = res.map(x => x.node.nodeValue).join("");
+
+  for (const match_it of full_text.matchAll(new RegExp(word, "g"))) {
+    const res_match = res.filter(res_it =>
+      (res_it.finish > match_it.index) && (res_it.start < match_it.index + word.length)
+    );
+
+    for (const res_it of res_match) {
+      let node_it = res_it.node;
+
+      if (res_it.finish > match_it.index + word.length) {
+        node_it.splitText(match_it.index + word.length - res_it.start);
+      }
+
+      if (res_it.start < match_it.index) {
+        node_it.splitText(match_it.index - res_it.start);
+        node_it = node_it.nextSibling as Text;
+      }
+
+      replace_with_multiple_nodes(node_it, [wrap(node_it.nodeValue!)]);
+    }
+  }
+
+  return body.innerHTML;
+}
+
+function parse_body(s: string): HTMLElement {
   const parser = new DOMParser();
   const root = parser.parseFromString(s, "text/html");
-  const que: Node[] = [root.body];
-  const res: Node[] = [];
+  return root.body;
+}
+
+function* iterate_deep(node: Node): Generator<Node> {
+  const que: Node[] = [node];
 
   while (que.length > 0) {
     const node_it = que.pop()!;
@@ -11,12 +76,17 @@ export function text_content(s: string): string {
       que.push(child_it);
     }
 
-    if (node_it.nodeType !== Node.TEXT_NODE) {
-      continue;
-    }
+    yield node_it;
+  }
+}
 
-    res.push(node_it);
+function replace_with_multiple_nodes(
+  node: Node,
+  nodes: Node[],
+) {
+  for (const node_it of nodes) {
+    node.parentNode!.insertBefore(node_it, node);
   }
 
-  return res.map(x => x.nodeValue).join("");
+  node.parentNode!.removeChild(node);
 }
