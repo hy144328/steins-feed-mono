047b402389faf9dbeb02a6b153cb73a507c43ad4
Hans Yu <hans.yu@outlook.de>
2025-02-05 11:08:33 +0100

    More tests.

--- steins-feed-ui/__tests__/util.test.ts
+++ steins-feed-ui/__tests__/util.test.ts
@@ -1,5 +1,16 @@
-import { join } from "@util"
+import {
+  join,
+  left_pad,
+} from "@util"
 
-test("join interleaves string array with comma", () => {
+test("join() interleaves string array with comma", () => {
   expect(join(["a", "b"], ",")).toEqual(["a", ",", "b"]);
+});
+
+test("left_pad() pads zeros until full", () => {
+  expect(left_pad(5, 4)).toBe("0005");
+});
+
+test("left_pad() pads zeros if necessary", () => {
+  expect(left_pad(5000, 4)).toBe("5000");
 });
