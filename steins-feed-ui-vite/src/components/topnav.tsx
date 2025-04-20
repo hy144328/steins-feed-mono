f43621f4ef0582b6c3d4542887d793e188d79bb2
Hans Yu <hans.yu@outlook.de>
2025-04-20 18:05:52 +0200

    First draft of top navigation.

---
+++ steins-feed-ui-vite/src/components/topnav.tsx
@@ -0,0 +1,142 @@
+import { NavLink, useNavigate } from "react-router"
+
+import { NavigationSearchParams, toURLSearchParams } from "@/util"
+
+export function TopNav(
+{
+  now,
+  languages,
+  tags,
+  wall_mode,
+  contentServed = false,
+}: NavigationSearchParams & {
+  contentServed?: boolean,
+}) {
+  return (
+<nav className="navbar bg-dark sticky-top" data-bs-theme="dark">
+  <div className="container">
+    <NavLink to="/" className="navbar-brand">
+      <img
+        src="/apple-touch-icon.png"
+        height={ 30 }
+        width={ 30 }
+        alt="Stein's Feed logo"
+        className="d-inline-block align-text-top"
+      />
+      &ensp;
+      <span className="d-none d-sm-inline-block">Stein&apos;s Feed</span>
+    </NavLink>
+
+    <ul className="nav nav-underline">
+      <li className="nav-item">
+        <NavLink
+          className="nav-link active"
+          to={ contentServed ? `/?${toURLSearchParams({now, languages, tags, wall_mode}).toString()}` : "/" }
+        >
+          Home
+        </NavLink>
+      </li>
+
+      <li className="nav-item">
+        <a className="nav-link disabled">
+          Settings
+        </a>
+      </li>
+
+      <li className="nav-item">
+        <a className="nav-link disabled">
+          Statistics
+        </a>
+      </li>
+    </ul>
+
+    <ul className="nav">
+      <li className="nav-item">
+        <NavigationPad
+          now={ now }
+          languages={ languages }
+          tags={ tags }
+          wall_mode={ wall_mode }
+          contentServed={ contentServed }
+        />
+      </li>
+    </ul>
+
+    <ul className="nav">
+      <li className="nav-item">
+        <LogoutButton/>
+      </li>
+
+      <li className="nav-item">
+        <button
+          className="btn btn-primary"
+          data-bs-toggle="offcanvas"
+          data-bs-target="#sidenav-offcanvas"
+          disabled={ !contentServed }
+        >
+          <i className="bi-list"/>
+        </button>
+      </li>
+    </ul>
+  </div>
+</nav>
+  );
+}
+
+function NavigationPad(
+{
+  now,
+  languages,
+  tags,
+  wall_mode,
+  contentServed = true,
+}: NavigationSearchParams & {
+  contentServed?: boolean,
+}) {
+  const yesterday = new Date(now);
+  yesterday.setUTCDate(now.getUTCDate() - 1);
+
+  const tomorrow = new Date(now);
+  tomorrow.setUTCDate(now.getUTCDate() + 1);
+
+  return (
+<div className="btn-group">
+  <a
+    className={ ["btn", "btn-primary"].concat(contentServed ? [] : ["disabled"]).join(" ") }
+    href={ contentServed ? `/?${toURLSearchParams({now: tomorrow, languages, tags, wall_mode}).toString()}` : undefined }
+  >
+    <i className="bi-rewind-fill"/>
+  </a>
+
+  <button className="btn btn-primary" disabled={ true }>
+    <i className="bi-caret-up-fill"/>
+  </button>
+
+  <button className="btn btn-primary" disabled={ true }>
+    <i className="bi-caret-down-fill"/>
+  </button>
+
+  <a
+    className={ ["btn", "btn-primary"].concat(contentServed ? [] : ["disabled"]).join(" ") }
+    href={ contentServed ? `/?${toURLSearchParams({now: yesterday, languages, tags, wall_mode}).toString()}` : undefined }
+  >
+    <i className="bi-fast-forward-fill"/>
+  </a>
+</div>
+  )
+}
+
+function LogoutButton() {
+  const navigate = useNavigate();
+
+  async function handleLogout() {
+    await logout();
+    navigate("/login");
+  }
+
+  return (
+    <button className="btn btn-danger">
+    <i className="bi-power" onClick={ handleLogout }/>
+    </button>
+  );
+}
