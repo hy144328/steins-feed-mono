ba3083f58f10891047034e365d949d978de87cef
Hans Yu <hans.yu@outlook.de>
2025-04-21 12:03:26 +0200

    Refactor router.

--- steins-feed-ui-vite/src/main.tsx
+++ steins-feed-ui-vite/src/main.tsx
@@ -1,10 +1,14 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import { BrowserRouter } from "react-router"
+import { BrowserRouter, Route, Routes } from "react-router"
 import App from './App.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <BrowserRouter><App /></BrowserRouter>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={ <App /> } />
+      </Routes>
+    </BrowserRouter>
   </StrictMode>,
 )
