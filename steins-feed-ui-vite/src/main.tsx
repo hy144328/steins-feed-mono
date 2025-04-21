5a780429f2d0e49c630a28791fbbb3725dc8af37
Hans Yu <hans.yu@outlook.de>
2025-04-21 12:44:46 +0200

    Add route.

--- steins-feed-ui-vite/src/main.tsx
+++ steins-feed-ui-vite/src/main.tsx
@@ -1,13 +1,16 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { BrowserRouter, Route, Routes } from "react-router"
-import App from './App.tsx'
+
+import App from '@/App'
+import LoginPage from "@/pages/login"
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <BrowserRouter>
       <Routes>
         <Route path="/" element={ <App /> } />
+        <Route path="/login/" element={ <LoginPage /> } />
       </Routes>
     </BrowserRouter>
   </StrictMode>,
