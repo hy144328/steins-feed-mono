6e664a4dea0402e7ffff049f36349ce87f9fc3c9
Hans Yu <hans.yu@outlook.de>
2025-04-27 02:03:35 +0200

    Edit distances.

---
+++ steins-feed-ui/src/metrics.ts
@@ -0,0 +1,41 @@
+export function similar_edit(
+  s: string,
+  t: string,
+  thresh: number,
+  i: number = 0,
+  j: number = 0,
+): boolean {
+  if (thresh < 0) {
+    return false;
+  }
+
+  if (i === s.length) {
+    return t.length <= thresh;
+  }
+
+  if (j === t.length) {
+    return s.length <= thresh;
+  }
+
+  if (s[i] === t[j]) {
+    return similar_edit(s, t, thresh, i+1, j+1);
+  }
+
+  return similar_edit(s, t, thresh-1, i+1, j) || similar_edit(s, t, thresh-1, i, j+1) || similar_edit(s, t, thresh-1, i+1, j+1);
+}
+
+export function similar_prefix(
+  s: string,
+  t: string,
+  thresh: number,
+): boolean {
+  if (s.length < thresh) {
+    return false;
+  }
+
+  if (t.length < thresh) {
+    return false;
+  }
+
+  return s.slice(0, thresh) === t.slice(0, thresh);
+}
