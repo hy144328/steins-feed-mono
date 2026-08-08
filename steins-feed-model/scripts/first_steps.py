879ed9e590570d45b0dcd16f873d8fd707791870
Hans Yu <github@shauny.anonaddy.me>
2026-08-08 19:11:09 +0200

    refactor(spark-etl-model): Refactor initialization.

--- steins-feed-model/scripts/first_steps.py
+++ steins-feed-model/scripts/first_steps.py
@@ -3,17 +3,20 @@
 import os
 
 import dotenv
+import sqlalchemy as sqla
 
 import steins_feed_model
 import steins_feed_model.base
 
 dotenv.load_dotenv()
 
-engine = steins_feed_model.EngineFactory.create_engine(
+url = sqla.URL.create(
+    "sqlite",
     username = os.getenv("DB_USER"),
     password = os.getenv("DB_PASS"),
     host = os.getenv("DB_HOST"),
-    port = os.getenv("DB_PORT"),
+    port = int(os.environ["DB_PORT"]) if "DB_PORT" in os.environ else None,
     database = os.getenv("DB_NAME"),
 )
+engine = sqla.create_engine(url)
 steins_feed_model.base.Base.metadata.create_all(engine)
--- steins-feed-model/steins_feed_model/__init__.py
+++ steins-feed-model/steins_feed_model/__init__.py
@@ -1,42 +1,6 @@
-import typing
-
 import sqlalchemy as sqla
 
 from . import feeds, items, users
-
-class EngineFactory:
-    @classmethod
-    def create_engine(
-        cls,
-        drivername: str = "sqlite",
-        username: typing.Optional[str] = None,
-        password: typing.Optional[str] = None,
-        host: typing.Optional[str] = None,
-        port: typing.Optional[typing.Union[int, str]] = None,
-        database: typing.Optional[str] = None,
-    ) -> sqla.engine.Engine:
-        url = sqla.URL.create(
-            drivername,
-            username = username,
-            password = password,
-            host = host,
-            port = int(port) if port is not None else None,
-            database = database,
-        )
-        connect_args = {
-            "check_same_thread": False,
-            "timeout": 5,
-        }
-        return sqla.create_engine(
-            url,
-            connect_args = connect_args,
-        )
-
-    @classmethod
-    def create_metadata(cls, engine: sqla.engine.Engine) -> sqla.MetaData:
-        metadata = sqla.MetaData()
-        metadata.reflect(bind=engine)
-        return metadata
 
 # SQLite check foreign keys.
 @sqla.event.listens_for(sqla.engine.Engine, "connect")
