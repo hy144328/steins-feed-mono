9229171cc73a8a74475505710cb8589cb80d9eaf
Hans Yu <github@shauny.anonaddy.me>
2026-08-08 21:01:54 +0200

    refactor(steins-feed-config): Update first-steps script.

--- steins-feed-config/scripts/first_steps.py
+++ steins-feed-config/scripts/first_steps.py
@@ -11,7 +11,7 @@ import sqlalchemy.exc as sqla_exc
 import sqlalchemy.orm as sqla_orm
 
 import steins_feed_config
-import steins_feed_model
+import steins_feed_model.base
 import steins_feed_model.users
 
 dotenv.load_dotenv()
@@ -21,39 +21,44 @@ logger = logging.getLogger()
 with open(os.path.join(os.path.dirname(__file__), "first_steps_logging.toml"), "rb") as f:
     logging.config.dictConfig(tomllib.load(f))
 
-engine = steins_feed_model.EngineFactory.create_engine(
+url = sqla.URL.create(
+    "sqlite",
     username = os.getenv("DB_USER"),
     password = os.getenv("DB_PASS"),
     host = os.getenv("DB_HOST"),
-    port = os.getenv("DB_PORT"),
+    port = int(os.environ["DB_PORT"]) if "DB_PORT" in os.environ else None,
     database = os.getenv("DB_NAME"),
 )
+engine = sqla.create_engine(url)
+steins_feed_model.base.Base.metadata.create_all(engine)
 
 pwd_context = passlib.context.CryptContext(schemes=["bcrypt"], deprecated="auto")
 
 with sqla_orm.Session(engine) as session:
     try:
-        user = steins_feed_model.users.User(
-            name = os.environ["DEV_USER"],
-            password = pwd_context.hash(os.environ["DEV_PASS"]),
-            email = os.environ["DEV_MAIL"],
-        )
-        session.add(user)
-        session.commit()
+        with session.begin():
+            user = steins_feed_model.users.User(
+                name = os.environ["DEV_USER"],
+                password = pwd_context.hash(os.environ["DEV_PASS"]),
+                email = os.environ["DEV_MAIL"],
+            )
+            session.add(user)
+            user_id = user.id
     except sqla_exc.IntegrityError:
         logger.warning(f"User {os.environ["DEV_USER"]} already exists.")
-        session.rollback()
 
-        q = sqla.select(
-            steins_feed_model.users.User,
-        ).where(
-            steins_feed_model.users.User.name == os.environ["DEV_USER"],
-        )
-        user = session.execute(q).scalars().one()
+        with session.begin():
+            q = sqla.select(
+                steins_feed_model.users.User,
+            ).where(
+                steins_feed_model.users.User.name == os.environ["DEV_USER"],
+            )
+            user = session.execute(q).scalars().one()
+            user_id = user.id
 
     with open("feeds.xml", "r") as f:
         steins_feed_config.read_xml(
             session,
             f,
-            user_id = user.id,
+            user_id = user_id,
         )
