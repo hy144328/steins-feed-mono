a90fa973283a3a65381df7d2e0328d4b847223a3
Hans Yu <github@shauny.anonaddy.me>
2026-08-09 22:13:10 +0200

    test(steins-feed-etl,steins-feed-magic): Improve coverage.

--- steins-feed-magic/tests/test_stem.py
+++ steins-feed-magic/tests/test_stem.py
@@ -5,3 +5,8 @@ def test_english():
     vect = steins_feed_magic.stem.StemmingCountVectorizer(steins_feed_model.feeds.Language.ENGLISH)
     tokenize = vect.build_tokenizer()
     assert tokenize("gives giving bottles") == ["give", "give", "bottl"]
+
+def test_english_no_stem():
+    vect = steins_feed_magic.stem.StemmingCountVectorizer(steins_feed_model.feeds.Language.ENGLISH)
+    tokenize = vect.build_tokenizer(skip_stem=True)
+    assert tokenize("gives giving bottles") == ["gives", "giving", "bottles"]
