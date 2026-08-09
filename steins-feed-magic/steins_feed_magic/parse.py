af7510ec5c0146a623b244cd1f9b4689afa49733
Hans Yu <github@shauny.anonaddy.me>
2026-08-09 22:12:45 +0200

    refactor(steins-feed-magic): Improve nomenclature.

--- steins-feed-magic/steins_feed_magic/parse.py
+++ steins-feed-magic/steins_feed_magic/parse.py
@@ -2,17 +2,17 @@ import lxml.etree
 import lxml.html
 
 def text_content(s: str) -> str:
-    queue: list[lxml.html.HtmlElement | str] = []
+    stack: list[lxml.html.HtmlElement | str] = []
     res: list[str] = []
 
     try:
         tree = lxml.html.fromstring(s)
-        queue.append(tree)
+        stack.append(tree)
     except lxml.etree.ParserError:  # pragma: no cover
         return ""
 
-    while len(queue) > 0:
-        node_it = queue.pop()
+    while len(stack) > 0:
+        node_it = stack.pop()
 
         if isinstance(node_it, str):
             res.append(node_it)
@@ -20,11 +20,11 @@ def text_content(s: str) -> str:
 
         for child_it in reversed(node_it):
             if child_it.tail is not None:
-                queue.append(child_it.tail)
+                stack.append(child_it.tail)
 
-            queue.append(child_it)
+            stack.append(child_it)
 
         if node_it.text is not None:
-            queue.append(node_it.text)
+            stack.append(node_it.text)
 
     return "".join(res)
