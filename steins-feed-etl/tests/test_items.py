c08254e4616907511ebb9249514b35dc275770f0
Hans Yu <github@shauny.anonaddy.me>
2026-08-12 23:51:06 +0200

    test(steins-feed-etl): Mock async client session.

--- steins-feed-etl/tests/test_items.py
+++ steins-feed-etl/tests/test_items.py
@@ -3,7 +3,9 @@ import tempfile
 import typing
 
 import aiohttp
+import aioresponses
 import pytest
+import pytest_asyncio
 import sqlalchemy as sqla
 import sqlalchemy.orm as sqla_orm
 
@@ -22,15 +24,38 @@ def engine() -> sqla.engine.Engine:
 def Session(engine: sqla.engine.Engine) -> sqla_orm.sessionmaker[sqla_orm.Session]:
     return sqla_orm.sessionmaker(engine)
 
-@pytest.fixture
-def temp_dir() -> collections.abc.Generator[str]:
-    with tempfile.TemporaryDirectory() as temp_dir:
-        yield temp_dir
+@pytest_asyncio.fixture
+async def client() -> collections.abc.AsyncGenerator[aiohttp.ClientSession]:
+    with aioresponses.aioresponses() as m:
+        m.get(
+            "https://www.theguardian.com/uk/rss",
+            status = 200,
+            body = b"""
+<?xml version="1.0" encoding="utf-8"?>
+<rss xmlns:media="http://search.yahoo.com/mrss/" xmlns:dc="http://purl.org/dc/elements/1.1/" version="2.0">
+  <channel>
+    <title>The Guardian</title>
+    <link>https://www.theguardian.com/uk</link>
+    <pubDate>Wed, 12 Aug 2026 20:51:33 GMT</pubDate>
+    <item>
+      <title>How the Other Half Loves review</title>
+      <link>https://www.theguardian.com/stage/2026/aug/12/how-the-other-half-loves-review-old-vic-london-alan-ayckbourn-roger-allam</link>
+      <description>The Old Vic, London</description>
+      <pubDate>Wed, 12 Aug 2026 17:00:34 GMT</pubDate>
+    </item>
+  </channel>
+</rss>
+            """,
+        )
+
+        async with aiohttp.ClientSession() as client:
+            yield client
 
 @pytest.fixture
-def temp_file(temp_dir: str) -> collections.abc.Generator[typing.TextIO]:
-    with tempfile.NamedTemporaryFile("w", dir=temp_dir, delete=False) as f:
-        f.write("""
+def temp_file() -> collections.abc.Generator[typing.TextIO]:
+    with tempfile.TemporaryDirectory() as temp_dir:
+        with tempfile.NamedTemporaryFile("w", dir=temp_dir, delete=False) as f:
+            f.write("""
 <root>
   <feed>
     <title>The Guardian</title>
@@ -38,15 +63,78 @@ def temp_file(temp_dir: str) -> collections.abc.Generator[typing.TextIO]:
     <lang>English</lang>
   </feed>
 </root>
-        """)
+            """)
 
-    with open(f.name, "r") as f:
-        yield f
+        with open(f.name, "r") as f:
+            yield f
+
+@pytest.mark.asyncio
+async def test_parse_feeds(
+    Session: sqla_orm.sessionmaker[sqla_orm.Session],
+    client: aiohttp.ClientSession,
+    temp_file: typing.TextIO,
+):
+    with Session() as session:
+        steins_feed_config.read_xml(session, temp_file, user=None)
+
+    await steins_feed_etl.parse_feeds(Session, client)
+
+    q = sqla.select(steins_feed_model.items.Item)
+    res = session.scalars(q).all()
+    assert len(res) == 1
+
+@pytest_asyncio.fixture
+async def client_long() -> collections.abc.AsyncGenerator[aiohttp.ClientSession]:
+    with aioresponses.aioresponses() as m:
+        m.get(
+            "https://www.theguardian.com/uk/rss",
+            status = 200,
+            body = b"""
+<?xml version="1.0" encoding="utf-8"?>
+<rss xmlns:media="http://search.yahoo.com/mrss/" xmlns:dc="http://purl.org/dc/elements/1.1/" version="2.0">
+  <channel>
+    <title>The Guardian</title>
+    <link>https://www.theguardian.com/uk</link>
+    <pubDate>Wed, 12 Aug 2026 20:51:33 GMT</pubDate>
+    <item>
+      <title>How the Other Half Loves review</title>
+      <link>https://www.theguardian.com/stage/2026/aug/12/how-the-other-half-loves-review-old-vic-london-alan-ayckbourn-roger-allam</link>
+      <description>The Old Vic, London</description>
+      <pubDate>Wed, 12 Aug 2026 17:00:34 GMT</pubDate>
+    </item>
+  </channel>
+</rss>
+            """,
+        )
+        m.get(
+            "https://www.theguardian.com/uk/culture/rss",
+            status = 200,
+            body = b"""
+<?xml version="1.0" encoding="utf-8"?>
+<rss xmlns:media="http://search.yahoo.com/mrss/" xmlns:dc="http://purl.org/dc/elements/1.1/" version="2.0">
+  <channel>
+    <title>The Guardian</title>
+    <link>https://www.theguardian.com/uk</link>
+    <pubDate>Wed, 12 Aug 2026 20:51:33 GMT</pubDate>
+    <item>
+      <title>How the Other Half Loves review</title>
+      <link>https://www.theguardian.com/stage/2026/aug/12/how-the-other-half-loves-review-old-vic-london-alan-ayckbourn-roger-allam</link>
+      <description>The Old Vic, London</description>
+      <pubDate>Wed, 12 Aug 2026 17:00:34 GMT</pubDate>
+    </item>
+  </channel>
+</rss>
+            """,
+        )
+
+        async with aiohttp.ClientSession() as client:
+            yield client
 
 @pytest.fixture
-def temp_file_long(temp_dir: str) -> collections.abc.Generator[typing.TextIO]:
-    with tempfile.NamedTemporaryFile("w", dir=temp_dir, delete=False) as f:
-        f.write("""
+def temp_file_long() -> collections.abc.Generator[typing.TextIO]:
+    with tempfile.TemporaryDirectory() as temp_dir:
+        with tempfile.NamedTemporaryFile("w", dir=temp_dir, delete=False) as f:
+            f.write("""
 <root>
   <feed>
     <title>The Guardian</title>
@@ -74,52 +162,37 @@ def temp_file_long(temp_dir: str) -> collections.abc.Generator[typing.TextIO]:
     <lang>English</lang>
   </feed>
 </root>
-        """)
+            """)
 
-    with open(f.name, "r") as f:
-        yield f
-
-@pytest.mark.asyncio
-async def test_parse_feeds(
-    Session: sqla_orm.sessionmaker[sqla_orm.Session],
-    temp_file: typing.TextIO,
-):
-    with Session() as session:
-        steins_feed_config.read_xml(session, temp_file, user=None)
-
-    async with aiohttp.ClientSession() as client:
-        await steins_feed_etl.parse_feeds(Session, client)
-
-    q = sqla.select(steins_feed_model.items.Item)
-    res = session.scalars(q).all()
-    assert len(res) > 0
+        with open(f.name, "r") as f:
+            yield f
 
 @pytest.mark.asyncio
 async def test_parse_feeds_long(
     Session: sqla_orm.sessionmaker[sqla_orm.Session],
+    client_long: aiohttp.ClientSession,
     temp_file_long: typing.TextIO,
 ):
     with Session() as session:
         steins_feed_config.read_xml(session, temp_file_long, user=None)
 
-    async with aiohttp.ClientSession() as client:
-        await steins_feed_etl.parse_feeds(Session, client)
+    await steins_feed_etl.parse_feeds(Session, client_long, batch_size=1)
 
     q = sqla.select(steins_feed_model.items.Item)
     res = session.scalars(q).all()
-    assert len(res) > steins_feed_etl.BATCH_SIZE
+    assert len(res) == 2
 
 @pytest.mark.asyncio
 async def test_parse_feeds_pattern(
     Session: sqla_orm.sessionmaker[sqla_orm.Session],
+    client_long: aiohttp.ClientSession,
     temp_file_long: typing.TextIO,
 ):
     with Session() as session:
         steins_feed_config.read_xml(session, temp_file_long, user=None)
 
-    async with aiohttp.ClientSession() as client:
-        await steins_feed_etl.parse_feeds(Session, client, title_pattern="Culture")
+    await steins_feed_etl.parse_feeds(Session, client_long, title_pattern="Culture")
 
     q = sqla.select(steins_feed_model.items.Item)
     res = session.scalars(q).all()
-    assert len(res) > 0
+    assert len(res) == 1
