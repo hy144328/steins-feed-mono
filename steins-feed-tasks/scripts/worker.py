25d341afef3fabb393f2c92f5fa10bb6c47bfc51
Hans Yu <github@shauny.anonaddy.me>
2026-09-14 22:15:54 +0200

    feat(steins-feed-tasks): Streamline app.

--- steins-feed-tasks/scripts/worker.py
+++ steins-feed-tasks/scripts/worker.py
@@ -1,12 +1,16 @@
 #!/usr/bin/env python3
 
+import logging.config
 import sys
+import tomllib
 
 import dotenv
 
 dotenv.load_dotenv()
 
+with open("logging.toml", "rb") as f:
+    logging.config.dictConfig(tomllib.load(f))
+
 import steins_feed_tasks.celery
 
-print(sys.argv)
 steins_feed_tasks.celery.app.worker_main(["worker"] + sys.argv[1:])
